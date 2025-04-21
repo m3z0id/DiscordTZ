06be886f4b72c0610eb5b313baa6a3561cf84629
vima <bigm3z0id@gmail.com>
2025-04-21 21:34:43 +0200

    Added get command

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ client: commands.Bot = commands.Bot("tz!", help_command=None, intents=discord.In
 
 db: dict = config.get("mariadbDetails")
 
-"""
 conn: mariadb.Connection = mariadb.connect(
     database=db.get("database"),
     user=db.get("user"),
@@ -20,7 +19,6 @@ conn: mariadb.Connection = mariadb.connect(
     port=int(db.get("port")),
     autocommit=bool(db.get("autocommit"))
 )
-"""
 
 success: discord.Embed = discord.Embed(
     title="**Success!**",
@@ -31,7 +29,7 @@ success: discord.Embed = discord.Embed(
 fail: discord.Embed = discord.Embed(
     title="**Something went wrong.**",
     description="There was an error in the operation.",
-    color=discord.Color.green()
+    color=discord.Color.red()
 )
 
 def fetchTimezones() -> list[dict[str: str]]:
@@ -47,6 +45,7 @@ def fetchTimezones() -> list[dict[str: str]]:
     return files
 
 timezones: list[dict[str: str]] = fetchTimezones()
+checkList: list[str] = [tz['area'] + "/" + tz["city"] for tz in timezones]
 mytimezone = app_commands.Group(name="mytimezone", description="Timezone related stuff")
 
 @client.event
@@ -65,13 +64,13 @@ async def getTimezones(ctx: discord.Interaction, current: str) -> list[app_comma
     cityMatches = [
         app_commands.Choice(name=f"{tz['area']}/{tz['city']}", value=f"{tz['area']}/{tz['city']}")
         for tz in timezones
-        if str(tz.get("city", "")).startswith(current)
+        if str(tz.get("city", "")).lower().startswith(current.lower())
     ]
 
     areaMatches = [
         app_commands.Choice(name=f"{tz['area']}/{tz['city']}", value=f"{tz['area']}/{tz['city']}")
         for tz in timezones
-        if str(tz.get("area", "")).startswith(current)
+        if str(tz.get("area", "")).lower().startswith(current.lower())
     ]
 
     for choice in cityMatches[:25]:
@@ -88,30 +87,66 @@ async def getTimezones(ctx: discord.Interaction, current: str) -> list[app_comma
 @mytimezone.command(name="set", description="Sets your timezone to the correct one.")
 @app_commands.describe(timezone="The timezone you are in.")
 @app_commands.autocomplete(timezone=getTimezones)
-async def set(ctx: discord.Interaction, timezone: app_commands.Choice[str]) -> None:
-    #cursor: mariadb.Cursor = conn.cursor(prepared=True)
-    query: str = f"INSERT into {db.get("tableName")} (user, timezone) VALUES (%s, %s)"
-
-    data: tuple[int, str] = (ctx.user.id, timezone.value)
-
-    try:
-        #cursor.execute(query, data)
-        #conn.commit()
-        print(f"Inserted {data} successfully!")
-        successCpy = success
-        successCpy.set_footer(ctx.user.name, ctx.user.avatar.url)
-        successCpy.timestamp = datetime.datetime.now()
-
-        ctx.response.send_message(embed=successCpy, ephemeral=True)
-
-    except mariadb.Error as err: 
-        print(f"Error: {err}")
-        print(f"Data: {data}")
+async def set(ctx: discord.Interaction, timezone: str) -> None:
+    nowtime: str = datetime.datetime.now().strftime("%d.%m.%y %H:%M:%S")
+    if(timezone not in checkList):
+        print(f"[{nowtime}] ERROR: {ctx.user} tried to set their timezone to {timezone}.")
         failCpy = fail
-        failCpy.set_footer(ctx.user.name, ctx.user.avatar.url)
+        failCpy.set_footer(text=ctx.user.name, icon_url=ctx.user.avatar.url)
         failCpy.timestamp = datetime.datetime.now()
 
-        ctx.response.send_message(embed=failCpy, ephemeral=True)
+        await ctx.response.send_message(embed=failCpy, ephemeral=True)
+        return
+
+    cursor: mariadb.Cursor = conn.cursor(prepared=True)
+    query: str = f"INSERT into {db.get("tableName")} (user, timezone) VALUES (%s, %s) ON DUPLICATE KEY UPDATE timezone = %s"
+
+    data: tuple[int, str, str] = (ctx.user.id, timezone.replace(" ", "_"), timezone.replace(" ", "_"))
+
+    try:
+        cursor.execute(query, data)
+        conn.commit()
+        print(f"[{nowtime}] SUCCESS: {ctx.user} set their timezone to {timezone}!")
+        successCpy = success
+        successCpy.set_footer(text=ctx.user.name, icon_url=ctx.user.avatar.url)
+        successCpy.timestamp = datetime.datetime.now()
+
+        await ctx.response.send_message(embed=successCpy, ephemeral=True)
+        return
+
+    except mariadb.Error as e: 
+        print(f"[{nowtime}] ERROR: {e}")
+        failCpy = fail
+        failCpy.set_footer(text=ctx.user.name, icon_url=ctx.user.avatar.url)
+        failCpy.timestamp = datetime.datetime.now()
+
+        await ctx.response.send_message(embed=failCpy, ephemeral=True)
+        return
+    
+@mytimezone.command(name="get", description="Shows you timezone you set.")
+async def get(ctx: discord.Interaction) -> None:
+    nowtime: str = datetime.datetime.now().strftime("%d.%m.%y %H:%M:%S")
+
+    cursor: mariadb.Cursor = conn.cursor(prepared=True)
+    query: str = f"SELECT timezone from {db.get("tableName")} WHERE user = %s"
+    data: list[int] = [ctx.user.id]
+
+    try:
+        cursor.execute(query, data)
+        conn.commit()
+
+        result = cursor.fetchone()
+
+        if(result):
+            tz: str = str(result[0])
+        else:
+            temp: list[str] = os.readlink("/etc/localtime").split("/")
+            tz: str = f"{temp[-2]}/{temp[-1]}"
+
+        await ctx.response.send_message(f"Your timezone is {tz.replace("_", " ")}", ephemeral=True)
+
+    except mariadb.Error as e:
+        print(f"[{nowtime}] ERROR: {e}")
 
 
 client.run(config["token"])
